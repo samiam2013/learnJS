ba771ee4c7db1315e1d16e78d74b7ff183f8dbda
Sam Myres <samiam2013@gmail.com>
2021-08-14 20:26:31 +0000

    add ex 2, fix instructions for 3

---
+++ Elementary/ex02_hello_name.js
@@ -0,0 +1,14 @@
+// Write  program that asks the user for their name and greets them
+//        with their name.
+
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+readline.question('Who are you? ', name => {
+	console.log(`Hey there ${name}`);
+	readline.close();
+});
+
+// thank you codeacademy.com. 
